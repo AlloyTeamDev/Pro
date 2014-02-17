0e68c63dca883ec33609369b5b849b93c7dfe0fb
yuanyan <yuanyan.cao@gmail.com>
2014-02-17 20:42:26 +0800

    Rework scrollfix.

--- js/scrollfix.js
+++ js/scrollfix.js
@@ -1,67 +1,51 @@
-/**
- * ScrollFix v0.1
- * http://www.joelambert.co.uk
- *
- * Copyright 2011, Joe Lambert.
- * Free to use under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- */
-
 // https://developers.google.com/mobile/articles/webapp_fixed_ui
 // https://github.com/filamentgroup/Overthrow/
 // http://bradfrostweb.com/blog/mobile/fixed-position/
 
-define(['./os'], function(){
+define(['./os', './event'], function(){
 
     var os = $.os;
     function scrollFix(borderHeight) {
 
-        var container = document.querySelector('.ui-page');
-        var content = container.querySelector('.js-page-content');
-
-        if(!content || !container) return;
+        var $page = $('.ui-page');
+        var $content = $('.js-page-content', $page);
+        if(!$content[0] || !$page[0]) return;
 
         // Variables to track inputs
-        var startY, startTopScroll;
+        var startTopScroll;
 
         // Handle the start of interactions
-        container.addEventListener('touchstart', function(event){
-            startY = event.touches[0].pageY;
-            startTopScroll = container.scrollTop;
+        $(document).on('touchstart', '.ui-page', function(event){
+            var page = event.currentTarget;
+            startTopScroll = page.scrollTop;
 
             if(startTopScroll <= 0)
-                container.scrollTop = 1;
+                page.scrollTop = 1;
 
-            if(startTopScroll + container.offsetHeight >= container.scrollHeight)
-                container.scrollTop = container.scrollHeight - container.offsetHeight - 1;
-        }, false);
+            if(startTopScroll + page.offsetHeight >= page.scrollHeight)
+                page.scrollTop = page.scrollHeight - page.offsetHeight - 1;
 
-        document.addEventListener('touchmove', function(event) {
-            if( content.clientHeight + borderHeight < container.clientHeight ||
-                content.clientWidth < container.clientWidth){
-                // your element have overflow
-                return event.preventDefault();
-            }
-
-            var curElement = event.target;
-            do{
-                var curElementClassName = curElement.className;
-                if(curElementClassName.indexOf('js-no-bounce') != -1 ){
-                    event.preventDefault();
-                    break;
+        }).on('touchmove', '.ui-page', function(event){
+                var page = event.currentTarget;
+                // TODO cache element select
+                var content = page.querySelector('.js-page-content');
+                if( content.clientHeight + borderHeight < page.clientHeight ||
+                    content.clientWidth < page.clientWidth){
+                    // your element have overflow
+                    return event.preventDefault();
                 }
-                curElement = curElement.parentNode;
-            }while(curElement && curElement !== document);
-        }, false);
+            })
     }
 
-    // Add ScrollFix only with iOS
+    // Add ScrollFix only for iOS
     if(os.ios >= 5 ) {
+        $('.js-no-bonce').on('touchmove', function(event){
+            event.preventDefault();
+        });
         // TODO: remove hard code
         scrollFix(50*2);
     }else{
         var html = document.documentElement;
         html.className = html.className + ' ' + 'js-no-overflow-scrolling';
     }
-
 })
