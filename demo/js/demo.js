09aa9165bedd7b86726ce9a5bb845f8520a9e429
元彦 <yuanyan.cao@gmail.com>
2014-02-19 16:34:25 +0800

    Lazyload demo fix.

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -91,7 +91,7 @@ require([
 
         }else if(tab == 'lazyload'){
             $('[data-lazy]').lazyload({
-                container: $.os > 5? $('#page5-container'): window
+                container: $.os.ios > 5? $('#page5-container'): window
             });
         }
 
